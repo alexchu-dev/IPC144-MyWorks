83a169c476aa790b704fe12405f6fb75c6105f35
Alex Chu <61229735+alexchu-dev@users.noreply.github.com>
2023-06-06 16:10:12 -0400

    Rewritten loops based on each case.

--- IPC-Workshops/WS04/p1/w4p1.c
+++ IPC-Workshops/WS04/p1/w4p1.c
@@ -19,7 +19,7 @@ piece of work is entirely of my own creation.
 
 int main(void)
 {
-    int numIteration, i;
+    int num, i;
     char loopType;
     printf("+----------------------+\n");
     printf("Loop application STARTED\n");
@@ -27,38 +27,39 @@ int main(void)
     do {
         printf("\nD = do/while | W = while | F = for | Q = quit\n");
         printf("Enter loop type and the number of times to iterate (Quit=Q0): ");
-        scanf(" %c%d", &loopType, &numIteration);
-
-        /* Haven't sort Q-1 yet, may have to rearrange not to determine and execute the numerical digit first*/
+        scanf(" %c%d", &loopType, &num);    //Recieving a char and a num as input. Intended space to avoid the buffer holds the return/space from prev input
+        i = num;  //Pre-assign counter i equals to num, so that it does not repeat assigning in the nest below.
+        
         if (loopType != 'Q') {
             if ((loopType != 'D') && (loopType != 'W') && (loopType != 'F'))
             {
                 printf("ERROR: Invalid entered value(s)!\n");
             }
-            else if (numIteration >= 3 && numIteration <= 20)
+            else if (num >= 3 && num <= 20)
             {
                 if (loopType == 'D')
                 {
                     printf("DO-WHILE: ");
-                    for (i = numIteration; i > 0; i--)
-                    {
+                    do {
                         printf("D");
-                    }
+                        i--;
+                    } while (i > 0);
                     printf("\n");
                 }
                 else if (loopType == 'W')
                 {
                     printf("WHILE   : ");
-                    for (i = numIteration; i > 0; i--)
+                    while ( i > 0)
                     {
                         printf("W");
+                        i--;
                     }
                     printf("\n");
                 }
                 else if (loopType == 'F')
                 {
                     printf("FOR     : ");
-                    for (i = numIteration; i > 0; i--)
+                    for (; i > 0; i--)
                     {
                         printf("F");
                     }
@@ -70,10 +71,10 @@ int main(void)
                 printf("ERROR: The number of iterations must be between 3-20 inclusive!\n");
             }
         }
-        else if (loopType == 'Q' && numIteration != 0) {
+        else if (loopType == 'Q' && num != 0) {
             printf("ERROR: To quit, the number of iterations should be 0!\n");
         }
-    } while (loopType != 'Q' || numIteration != 0);
+    } while (loopType != 'Q' || num != 0);
     printf("\n+--------------------+\n");
     printf("Loop application ENDED\n");
     printf("+--------------------+\n\n");
